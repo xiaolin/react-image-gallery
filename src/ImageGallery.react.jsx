2fca0fc6628965b0cb1e5bac4b29f6acc497e945
Xiao Lin <xiao@linxtion.com>
2016-02-13 12:16:26 -0500

    Removes semicolons/lint

--- src/ImageGallery.react.jsx
+++ src/ImageGallery.react.jsx
@@ -1,7 +1,5 @@
-'use strict';
-
-import React from 'react';
-import Swipeable from 'react-swipeable';
+import React from 'react'
+import Swipeable from 'react-swipeable'
 
 const ImageGallery = React.createClass({
 
@@ -38,7 +36,7 @@ const ImageGallery = React.createClass({
       startIndex: 0,
       disableScrolling: false,
       server: false
-    };
+    }
   },
 
   getInitialState() {
@@ -46,7 +44,7 @@ const ImageGallery = React.createClass({
       currentIndex: this.props.startIndex,
       thumbnailsTranslateX: 0,
       containerWidth: 0
-    };
+    }
   },
 
   componentDidUpdate(prevProps, prevState) {
@@ -60,7 +58,7 @@ const ImageGallery = React.createClass({
 
       this._setThumbnailsTranslateX(
         -this._getScrollX(this.state.currentIndex > 0 ? 1 : 0) *
-        this.state.currentIndex);
+        this.state.currentIndex)
 
     }
 
@@ -68,23 +66,23 @@ const ImageGallery = React.createClass({
 
       // call back function if provided
       if (this.props.onSlide) {
-        this.props.onSlide(this.state.currentIndex);
+        this.props.onSlide(this.state.currentIndex)
       }
 
       // calculates thumbnail container position
       if (this.state.currentIndex === 0) {
-        this._setThumbnailsTranslateX(0);
+        this._setThumbnailsTranslateX(0)
       } else {
         let indexDifference = Math.abs(
-          prevState.currentIndex - this.state.currentIndex);
-        let scrollX = this._getScrollX(indexDifference);
+          prevState.currentIndex - this.state.currentIndex)
+        let scrollX = this._getScrollX(indexDifference)
         if (scrollX > 0) {
           if (prevState.currentIndex < this.state.currentIndex) {
             this._setThumbnailsTranslateX(
-              this.state.thumbnailsTranslateX - scrollX);
+              this.state.thumbnailsTranslateX - scrollX)
           } else if (prevState.currentIndex > this.state.currentIndex) {
             this._setThumbnailsTranslateX(
-              this.state.thumbnailsTranslateX + scrollX);
+              this.state.thumbnailsTranslateX + scrollX)
           }
         }
       }
@@ -93,199 +91,199 @@ const ImageGallery = React.createClass({
   },
 
   componentWillMount() {
-    this._thumbnailDelay = 300;
-    this._ghotClickDelay = 600;
-    this._preventGhostClick = false;
+    this._thumbnailDelay = 300
+    this._ghotClickDelay = 600
+    this._preventGhostClick = false
   },
 
   componentDidMount() {
-    this._handleResize();
+    this._handleResize()
     if (this.props.autoPlay) {
-      this.play();
+      this.play()
     }
-    window.addEventListener('resize', this._handleResize);
+    window.addEventListener('resize', this._handleResize)
   },
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this._handleResize);
+    window.removeEventListener('resize', this._handleResize)
     if (this._intervalId) {
-      window.clearInterval(this._intervalId);
-      this._intervalId = null;
+      window.clearInterval(this._intervalId)
+      this._intervalId = null
     }
   },
 
   slideToIndex(index, event) {
-    let slideCount = this.props.items.length - 1;
+    let slideCount = this.props.items.length - 1
 
     if (index < 0) {
-      this.setState({currentIndex: slideCount});
+      this.setState({currentIndex: slideCount})
     } else if (index > slideCount) {
-      this.setState({currentIndex: 0});
+      this.setState({currentIndex: 0})
     } else {
-      this.setState({currentIndex: index});
+      this.setState({currentIndex: index})
     }
     if (event) {
       if (this._intervalId) {
         // user event, reset interval
-        this.pause();
-        this.play();
+        this.pause()
+        this.play()
       }
     }
   },
 
   play() {
     if (this._intervalId) {
-      return;
+      return
     }
     this._intervalId = window.setInterval(() => {
       if (!this.state.hovering) {
-        this.slideToIndex(this.state.currentIndex + 1);
+        this.slideToIndex(this.state.currentIndex + 1)
       }
-    }, this.props.slideInterval);
+    }, this.props.slideInterval)
   },
 
   pause() {
     if (this._intervalId) {
-      window.clearInterval(this._intervalId);
-      this._intervalId = null;
+      window.clearInterval(this._intervalId)
+      this._intervalId = null
     }
   },
 
   _wrapClick(func) {
     return event => {
       if (this._preventGhostClick === true) {
-        return;
+        return
       }
-      func(event);
+      func(event)
     }
   },
 
   _touchEnd(event) {
-    this._preventGhostClick = true;
+    this._preventGhostClick = true
     this._preventGhostClickTimer = window.setTimeout(_ => {
-      this._preventGhostClick = false;
-      this._preventGhostClickTimer = null;
+      this._preventGhostClick = false
+      this._preventGhostClickTimer = null
     }, this._ghotClickDelay)
   },
 
   _setThumbnailsTranslateX(x) {
-    this.setState({thumbnailsTranslateX: x});
+    this.setState({thumbnailsTranslateX: x})
   },
 
   _handleResize() {
-    this.setState({containerWidth: this._imageGallery.offsetWidth});
+    this.setState({containerWidth: this._imageGallery.offsetWidth})
   },
 
   _getScrollX(indexDifference) {
     if (this.props.disableScrolling) {
-      return 0;
+      return 0
     }
     if (this._thumbnails) {
       if (this._thumbnails.scrollWidth <= this.state.containerWidth) {
-        return 0;
+        return 0
       }
 
-      let totalThumbnails = this._thumbnails.children.length;
+      let totalThumbnails = this._thumbnails.children.length
 
       // total scroll-x required to see the last thumbnail
-      let totalScrollX = this._thumbnails.scrollWidth - this.state.containerWidth;
+      let totalScrollX = this._thumbnails.scrollWidth - this.state.containerWidth
 
       // scroll-x required per index change
-      let perIndexScrollX = totalScrollX / (totalThumbnails - 1);
+      let perIndexScrollX = totalScrollX / (totalThumbnails - 1)
 
-      return indexDifference * perIndexScrollX;
+      return indexDifference * perIndexScrollX
     }
   },
 
   _handleMouseOverThumbnails(index, event) {
-    this.setState({hovering: true});
+    this.setState({hovering: true})
     if (this._thumbnailTimer) {
-      window.clearTimeout(this._thumbnailTimer);
-      this._thumbnailTimer = null;
+      window.clearTimeout(this._thumbnailTimer)
+      this._thumbnailTimer = null
     }
     this._thumbnailTimer = window.setTimeout(() => {
-      this.slideToIndex(index);
-      this.pause();
+      this.slideToIndex(index)
+      this.pause()
     }, this._thumbnailDelay)
   },
 
   _handleMouseLeaveThumbnails() {
     if (this._thumbnailTimer) {
-      window.clearTimeout(this._thumbnailTimer);
-      this._thumbnailTimer = null;
+      window.clearTimeout(this._thumbnailTimer)
+      this._thumbnailTimer = null
       if (this.props.autoPlay == true) {
-        this.play();
+        this.play()
       }
     }
-    this.setState({hovering: false});
+    this.setState({hovering: false})
   },
 
   _handleMouseOver() {
-    this.setState({hovering: true});
+    this.setState({hovering: true})
   },
 
   _handleMouseLeave() {
-    this.setState({hovering: false});
+    this.setState({hovering: false})
   },
 
   _getAlignmentClassName(index) {
-    let currentIndex = this.state.currentIndex;
-    let alignment = '';
+    let currentIndex = this.state.currentIndex
+    let alignment = ''
     switch (index) {
       case (currentIndex - 1):
-        alignment = ' left';
-        break;
+        alignment = ' left'
+        break
       case (currentIndex):
-        alignment = ' center';
-        break;
+        alignment = ' center'
+        break
       case (currentIndex + 1):
-        alignment = ' right';
-        break;
+        alignment = ' right'
+        break
     }
 
     if (this.props.items.length >= 3) {
       if (index === 0 && currentIndex === this.props.items.length - 1) {
         // set first slide as right slide if were sliding right from last slide
-        alignment = ' right';
+        alignment = ' right'
       } else if (index === this.props.items.length - 1 && currentIndex === 0) {
         // set last slide as left slide if were sliding left from first slide
-        alignment = ' left';
+        alignment = ' left'
       }
     }
 
-    return alignment;
+    return alignment
   },
 
   _handleImageLoad(event) {
     if (event.target.className.indexOf('loaded') === -1) {
-      event.target.className += ' loaded';
+      event.target.className += ' loaded'
     }
   },
 
   _handleImageError(event) {
     if (this.props.defaultImage && event.target.src.indexOf(this.props.defaultImage) === -1) {
-      event.target.src = this.props.defaultImage;
+      event.target.src = this.props.defaultImage
     }
   },
 
   render() {
-    let currentIndex = this.state.currentIndex;
+    let currentIndex = this.state.currentIndex
     let thumbnailStyle = {
       MozTransform: 'translate3d(' + this.state.thumbnailsTranslateX + 'px, 0, 0)',
       WebkitTransform: 'translate3d(' + this.state.thumbnailsTranslateX + 'px, 0, 0)',
       OTransform: 'translate3d(' + this.state.thumbnailsTranslateX + 'px, 0, 0)',
       msTransform: 'translate3d(' + this.state.thumbnailsTranslateX + 'px, 0, 0)',
       transform: 'translate3d(' + this.state.thumbnailsTranslateX + 'px, 0, 0)'
-    };
+    }
 
-    let slides = [];
-    let thumbnails = [];
-    let bullets = [];
+    let slides = []
+    let thumbnails = []
+    let bullets = []
 
     this.props.items.map((item, index) => {
-      let alignment = this._getAlignmentClassName(index);
-      let originalClass = item.originalClass ? ' ' + item.originalClass : '';
-      let thumbnailClass = item.thumbnailClass ? ' ' + item.thumbnailClass : '';
+      let alignment = this._getAlignmentClassName(index)
+      let originalClass = item.originalClass ? ' ' + item.originalClass : ''
+      let thumbnailClass = item.thumbnailClass ? ' ' + item.thumbnailClass : ''
 
       let slide = (
         <div
@@ -302,14 +300,14 @@ const ImageGallery = React.createClass({
               onError={this._handleImageError}/>
             {item.description}
         </div>
-      );
+      )
 
       if (this.props.lazyLoad) {
         if (alignment) {
-          slides.push(slide);
+          slides.push(slide)
         }
       } else {
-        slides.push(slide);
+        slides.push(slide)
       }
 
       if (this.props.showThumbnails) {
@@ -332,7 +330,7 @@ const ImageGallery = React.createClass({
               alt={item.thumbnailAlt}
               onError={this._handleImageError}/>
           </a>
-        );
+        )
       }
 
       if (this.props.showBullets) {
@@ -347,13 +345,13 @@ const ImageGallery = React.createClass({
             onTouchEnd={this._touchEnd}
             onClick={this._wrapClick(this.slideToIndex.bind(this, index))}>
           </li>
-        );
+        )
       }
-    });
+    })
 
-    let swipePrev = this.slideToIndex.bind(this, currentIndex - 1);
-    let swipeNext = this.slideToIndex.bind(this, currentIndex + 1);
-    let itemsTotal = this.props.items.length;
+    let swipePrev = this.slideToIndex.bind(this, currentIndex - 1)
+    let swipeNext = this.slideToIndex.bind(this, currentIndex + 1)
+    let itemsTotal = this.props.items.length
     return (
       <section ref={(i) => this._imageGallery = i} className='image-gallery'>
         <div
@@ -428,10 +426,10 @@ const ImageGallery = React.createClass({
             </div>
         }
       </section>
-    );
+    )
 
   }
 
-});
+})
 
-export default ImageGallery;
+export default ImageGallery
