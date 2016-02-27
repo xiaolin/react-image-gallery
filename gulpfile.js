81962477c8e7edc720456702372d88cedb7f09d9
Xiao Lin <xiao@linxtion.com>
2016-02-27 12:43:05 -0500

    Lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,32 @@
-'use strict';
-
-var babelify = require('babelify');
-var babel = require('gulp-babel');
-var browserify = require('browserify');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
-var streamify = require('gulp-streamify');
-var uglify = require('gulp-uglify');
-var watchify = require('watchify');
+var babelify = require('babelify')
+var babel = require('gulp-babel')
+var browserify = require('browserify')
+var concat = require('gulp-concat')
+var connect = require('gulp-connect')
+var gulp = require('gulp')
+var livereload = require('gulp-livereload')
+var rename = require('gulp-rename')
+var sass = require('gulp-sass')
+var source = require('vinyl-source-stream')
+var streamify = require('gulp-streamify')
+var uglify = require('gulp-uglify')
+var watchify = require('watchify')
 
 gulp.task('server', function () {
   connect.server({
     root: ['example', 'build'],
     port: 8001,
     livereload: true
-  });
-});
+  })
+})
 
 gulp.task('sass', function () {
   gulp.src('./src/ImageGallery.scss')
     .pipe(sass())
     .pipe(rename('image-gallery.css'))
     .pipe(gulp.dest('./build/'))
-    .pipe(livereload());
-});
+    .pipe(livereload())
+})
 
 gulp.task('scripts', function() {
   watchify(browserify({
@@ -40,21 +38,21 @@ gulp.task('scripts', function() {
     .pipe(source('example.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./example/'))
-    .pipe(livereload());
-});
+    .pipe(livereload())
+})
 
 gulp.task('source-js', function () {
   return gulp.src('./src/ImageGallery.react.jsx')
     .pipe(concat('image-gallery.js'))
     .pipe(babel())
-    .pipe(gulp.dest('./build'));
-});
+    .pipe(gulp.dest('./build'))
+})
 
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch(['src/*.scss'], ['sass']);
-  gulp.watch(['src/*.jsx', 'example/app.js'], ['scripts']);
-});
+  livereload.listen()
+  gulp.watch(['src/*.scss'], ['sass'])
+  gulp.watch(['src/*.jsx', 'example/app.js'], ['scripts'])
+})
 
-gulp.task('dev', ['watch', 'scripts', 'sass', 'server']);
-gulp.task('build', ['source-js', 'sass']);
+gulp.task('dev', ['watch', 'scripts', 'sass', 'server'])
+gulp.task('build', ['source-js', 'sass'])
