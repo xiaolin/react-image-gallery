ccdc49bb7f55dd269486b3d12aa767f42699eb4e
Lukasz Kielczykowski <kielczykowski.lukasz@gmail.com>
2016-03-27 10:11:24 +0200

    Removes requiring utils - it doesn't exist anymore

--- src/javascripts/components/ImageGallery.es6.jsx
+++ src/javascripts/components/ImageGallery.es6.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Utils from '../utils';
-
 export default function ImageGalleryImporter(
   SlidesContainer,
   ThumbnailsContainer,
