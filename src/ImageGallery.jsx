d8c2cebdbf9bf4f6134dfb4757688a4e9f250dd4
sri harsha <sriharshaganja2@gmail.com>
2016-08-05 02:00:52 -0500

    Added capability to display labels for each thumbnail

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -582,11 +582,15 @@ export default class ImageGallery extends React.Component {
 
             onTouchStart={event => this.slideToIndex.call(this, index, event)}
             onClick={event => this.slideToIndex.call(this, index, event)}>
-
-            <img
-              src={item.thumbnail}
-              alt={item.thumbnailAlt}
-              onError={onThumbnailError.bind(this)}/>
+            <div style={{ width: '63px' }}>
+              <img
+                src={item.thumbnail}
+                alt={item.thumbnailAlt}
+                onError={onThumbnailError.bind(this)}/>
+               <span style={{ fontSize: '0.8em', color: 'black' }}>
+                 {item.label}
+               </span>
+            </div>
           </a>
         );
       }
