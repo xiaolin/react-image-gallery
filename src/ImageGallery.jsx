511b8eb898d603007ef1df7c7666d2d4edad512b
Jack Chu <jack@jackchu.com>
2017-01-05 22:44:01 -0500

    Add ability to pass custom react component nav elements as props

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -59,6 +59,8 @@ export default class ImageGallery extends React.Component {
     onImageError: React.PropTypes.func,
     onThumbnailError: React.PropTypes.func,
     renderCustomControls: React.PropTypes.func,
+    renderLeftNav: React.PropTypes.func,
+    renderRightNav: React.PropTypes.func,
     renderItem: React.PropTypes.func,
   };
 
@@ -82,6 +84,24 @@ export default class ImageGallery extends React.Component {
     startIndex: 0,
     slideDuration: 450,
     slideInterval: 3000,
+    renderLeftNav: (onClick, disabled) => {
+      return (
+        <button
+          type='button'
+          className='image-gallery-left-nav'
+          disabled={disabled}
+          onClick={onClick}/>
+      );
+    },
+    renderRightNav: (onClick, disabled) => {
+      return (
+        <button
+          type='button'
+          className='image-gallery-right-nav'
+          disabled={disabled}
+          onClick={onClick}/>
+      );
+    }
   };
 
   componentWillReceiveProps(nextProps) {
@@ -824,17 +844,8 @@ export default class ImageGallery extends React.Component {
             [
               this.props.showNav &&
                 <span key='navigation'>
-                  <button
-                    type='button'
-                    className='image-gallery-left-nav'
-                    disabled={!this._canSlideLeft()}
-                    onClick={slideLeft}/>
-
-                  <button
-                    type='button'
-                    className='image-gallery-right-nav'
-                    disabled={!this._canSlideRight()}
-                    onClick={slideRight}/>
+                  {this.props.renderLeftNav(slideLeft, !this._canSlideLeft())}
+                  {this.props.renderRightNav(slideRight, !this._canSlideRight())}
                 </span>,
 
                 this.props.disableSwipe ?
