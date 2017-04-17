bb21046694cf6624d93d436f157c2904fcc85b91
Xiao Lin <xiao@linxtion.com>
2017-04-16 21:07:11 -0400

    Fix slide from being stuck when moving down/up during swipe
    #174 On mobile you can swipe left or right and move your finger downwards, once you leave the container the slider stops with the image stuck half way.

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -932,6 +932,8 @@ export default class ImageGallery extends React.Component {
                     onSwiped={this._handleOnSwiped.bind(this)}
                     onSwipedLeft={this._handleOnSwipedTo.bind(this, 1)}
                     onSwipedRight={this._handleOnSwipedTo.bind(this, -1)}
+                    onSwipedDown={this._handleOnSwipedTo.bind(this, 0)}
+                    onSwipedUp={this._handleOnSwipedTo.bind(this, 0)}
                   >
                     <div className='image-gallery-slides'>
                       {slides}
