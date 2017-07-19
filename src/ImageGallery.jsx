0f8b42f678f092d51f9e4737991491620fe89067
4muzar <juumanji@gmail.com>
2017-07-19 19:41:40 +0300

    revert ie fix

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 
 const screenChangeEvents = [
   'fullscreenchange',
-  'MSFullscreenChange',
+  'msfullscreenchange',
   'mozfullscreenchange',
   'webkitfullscreenchange'
 ];
