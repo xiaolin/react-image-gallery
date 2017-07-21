fa5c2039fbf58702071ac946579b9061a7aa81f1
4muzar <juumanji@gmail.com>
2017-07-21 10:27:30 +0300

    fix ie fullscreen change event

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 
 const screenChangeEvents = [
   'fullscreenchange',
-  'msfullscreenchange',
+  'MSFullscreenChange',
   'mozfullscreenchange',
   'webkitfullscreenchange'
 ];
