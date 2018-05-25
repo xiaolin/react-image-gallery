532e266ec12af4d7a6c304f00d72bee6f6541bb6
Brian Emil Hartz <brianhartz@gmail.com>
2018-05-25 07:37:14 -0600

    update react-swipeable, clean up no-op funcs

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -542,12 +542,6 @@ export default class ImageGallery extends React.Component {
     return Math.abs(this.state.offsetPercentage) > this.props.swipeThreshold;
   }
 
-  _onSwipingNoOp() {
-    /*
-    Do nothing, only defined so preventDefaultTouchmoveEvent works
-    */
-  }
-
   _handleSwiping = (e, deltaX, deltaY, delta) => {
     const { galleryWidth, isTransitioning, scrollingUpDown } = this.state;
     const { swipingTransitionDuration } = this.props;
@@ -1084,10 +1078,6 @@ export default class ImageGallery extends React.Component {
                     delta={0}
                     flickThreshold={this.props.flickThreshold}
                     onSwiping={this._handleSwiping}
-                    onSwipingLeft={this._onSwipingNoOp}
-                    onSwipingRight={this._onSwipingNoOp}
-                    onSwipingUp={this._onSwipingNoOp}
-                    onSwipingDown={this._onSwipingNoOp}
                     onSwiped={this._handleOnSwiped}
                     stopPropagation={this.props.stopPropagation}
                     preventDefaultTouchmoveEvent={preventDefaultTouchmoveEvent || scrollingLeftRight}
