10198639de7ad53b0b2adf7a049535f8f910858d
Hilu luke <z585819@qq.com>
2019-02-13 23:46:13 +0800

    Trigger onSlide after slide transition (#380)

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -177,10 +177,6 @@ export default class ImageGallery extends React.Component {
       this._handleResize();
     }
     if (prevState.currentIndex !== this.state.currentIndex) {
-      if (this.props.onSlide) {
-        this.props.onSlide(this.state.currentIndex);
-      }
-
       this._updateThumbnailTranslate(prevState.currentIndex);
     }
 
@@ -351,6 +347,9 @@ export default class ImageGallery extends React.Component {
     this._transitionTimer = window.setTimeout(() => {
       if (isTransitioning) {
         this.setState({isTransitioning: !isTransitioning});
+        if (this.props.onSlide) {
+          this.props.onSlide(this.state.currentIndex);
+        }
       }
     }, this.props.slideDuration + 50);
   };
