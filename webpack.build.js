61e286f7b5ea48f7f7d5c4b8a1069e38b610062c
Xiao Lin <xiao@linxtion.com>
2021-08-10 20:50:36 -0700

    If externals build in webpack for amd #622

--- webpack.build.js
+++ webpack.build.js
@@ -36,13 +36,13 @@ const jsOutput = Object.assign({}, config, {
     react: {
       commonjs: 'react',
       commonjs2: 'react',
-      amd: 'React',
+      amd: 'react',
       root: 'React',
     },
     'react-dom': {
       commonjs: 'react-dom',
       commonjs2: 'react-dom',
-      amd: 'ReactDOM',
+      amd: 'react-dom',
       root: 'ReactDOM',
     },
   },
