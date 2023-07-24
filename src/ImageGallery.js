e0741414ffbb2cbc85c875c14e2b76bc1b4462c9
Alex Botham <alexbotham27@gmail.com>
2023-07-24 21:52:58 +0300

    Add vertical scrolling for image gallery

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -49,6 +49,7 @@ class ImageGallery extends React.Component {
       isFullscreen: false,
       isSwipingThumbnail: false,
       isPlaying: false,
+      slideVertically: props.slideVertically
     };
     this.loadedImages = {};
     this.imageGallery = React.createRef();
@@ -456,30 +457,30 @@ class ImageGallery extends React.Component {
 
     // calculates where the other slides belong based on currentIndex
     // if it is RTL the base line should be reversed
-    let translateX =
+    let translateValue =
       (baseTranslateX + index * 100) * (isRTL ? -1 : 1) + currentSlideOffset;
 
     if (infinite && items.length > 2) {
       if (currentIndex === 0 && index === totalSlides) {
         // make the last slide the slide before the first
         // if it is RTL the base line should be reversed
-        translateX = -100 * (isRTL ? -1 : 1) + currentSlideOffset;
+        translateValue = -100 * (isRTL ? -1 : 1) + currentSlideOffset;
       } else if (currentIndex === totalSlides && index === 0) {
         // make the first slide the slide after the last
         // if it is RTL the base line should be reversed
-        translateX = 100 * (isRTL ? -1 : 1) + currentSlideOffset;
+        translateValue = 100 * (isRTL ? -1 : 1) + currentSlideOffset;
       }
     }
 
     // Special case when there are only 2 items with infinite on
     if (infinite && items.length === 2) {
-      translateX = this.getTranslateXForTwoSlide(index);
+      translateValue = this.getTranslateXForTwoSlide(index);
     }
 
-    let translate = `translate(${translateX}%, 0)`;
+    let translate = slideVertically ? `translate(0, ${translateValue}%)` : `translate(${translateValue}%, 0)`;
 
     if (useTranslate3D) {
-      translate = `translate3d(${translateX}%, 0, 0)`;
+      translate = slideVertically ? `translate3d(0, ${translateValue}%, 0)` : `translate3d(${translateValue}%, 0, 0)`;
     }
 
     // don't show some slides while transitioning to avoid background transitions
@@ -1637,6 +1638,7 @@ ImageGallery.propTypes = {
   useTranslate3D: bool,
   isRTL: bool,
   useWindowKeyDown: bool,
+  slideVertically: bool
 };
 
 ImageGallery.defaultProps = {
@@ -1687,6 +1689,7 @@ ImageGallery.defaultProps = {
   slideInterval: 3000,
   slideOnThumbnailOver: false,
   swipeThreshold: 30,
+  slideVertically: false,
   renderLeftNav: (onClick, disabled) => (
     <LeftNav onClick={onClick} disabled={disabled} />
   ),
